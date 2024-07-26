5239d363b7098313728e550dc9a96c128d3c0845
Naman Jain <51355689+namanjain2008@users.noreply.github.com>
2024-07-26 23:39:43 +0200

    Creating functions for strucutral data processing

---
+++ structural_data_analysis.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#Following script has been made by Naman Jain with following features included in the
+#different version upgrades
+
+##It will be used to process strucutral data. Following steps will be taken to fix the data
+
+# for MGE, Bo distortion correction is needed
+
+
+Bo_MAP () {
+
+
+}
+
+
+
+#Linear Evolution of Phase
+LEP () {
+
+
+}
+
+
+BIAS_FIELD_CORRECTIOn () {
+
+    
+}
